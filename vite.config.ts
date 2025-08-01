8af46afb64ae86941b9b49cffed68c9d4ce224eb
Mau_2993 <shastrimayur2993@gmail.com>
2025-08-01 21:55:34 +0530

    Update: vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteStaticCopy({
+    targets: [
+      {
+        src: 'public/_redirects',
+        dest: '.', // Puts it at root of dist/
+      },
+    ],
+  }),],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  publicDir: 'public',
 });
