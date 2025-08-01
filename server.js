4a3c4d5ab324bcee62a07424a16cec3e656a1df1
Mau_2993 <shastrimayur2993@gmail.com>
2025-08-01 23:05:40 +0530

    Fix: Express version

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // SPA fallback: serve index.html for all unmatched routes
-app.get('/:path(*)', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
